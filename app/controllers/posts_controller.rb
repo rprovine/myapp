aaa63836d5243b4b35eef991b1157340a81426bf
Reno Provine <rprovine@gmail.com>
2013-12-24 11:15:06 -0600

    added rails precourse work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show 
@@ -15,7 +19,20 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post_params
+    else
+      render 'edit'
+    end
+  end
   private
     def post_params
       params.require(:post).permit(:title, :text)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :user
+
+  validates :title, presence: true,
+            length: { minimum: 5 }
 end
